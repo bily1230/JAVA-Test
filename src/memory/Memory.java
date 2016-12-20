4f094ec7b41e9411d1ffb67f099e5854b387a0f7
宁彬 <ningbing@homolo.com>
2016-12-20 14:24:20 +0800

    11

---
+++ src/memory/Memory.java
@@ -0,0 +1,37 @@
+package memory;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Memory {
+
+   
+	/**
+	 * 传地址 ,编程中多注意
+	 * @param args
+	 */
+    
+	public static void main(String[] args) {
+	
+		Map<String , List<String>> map = new HashMap<String, List<String>>();
+	    
+		List<String> list = new ArrayList<String>();
+		list.add("1");
+		list.add("2");
+		list.add("3");
+		map.put("1", list);
+		
+		List<String> listTest = map.get("1");
+		
+		listTest.add("4");
+		listTest.add("5");
+		
+		System.out.println(list.size());
+		System.out.println(map.get("1").size());
+		
+		
+	}
+
+}
